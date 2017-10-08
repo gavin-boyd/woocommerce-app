76537f62123cabe1cdffe4d0ad740ed54f59fc26
gavin-boyd-uuj <g.boyd@ulster.ac.uk>
2017-10-08 19:53:19 +0100

    angular project first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+##Project Node Dependencies
+
+1. Angular JS
+2. WooCommerce API NodeJs Wrapper - https://github.com/woocommerce/wc-api-node
+
+##Useful Resources
+
+- https://angular.io/guide
+- https://blog.risingstack.com/node-hero-node-js-project-structure-tutorial
+- https://github.com/angular/angular-seed (angular project boiler plate)
+
+##CMDs
---
+++ lswc/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductComponent } from './product/product.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  { path: 'crisis-center', component: CrisisListComponent },
+  { path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: 'heroes',
+    component: HeroListComponent,
+    data: { title: 'Heroes List' }
+  },
+  { path: '',
+    redirectTo: '/heroes',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
